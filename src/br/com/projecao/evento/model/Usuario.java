77195250b2c13605b1330c175d3197c7c9143b20
Luiz Henrique <luizsnip@gmail.com>
2016-06-13 13:09:30 -0300

    Atualização do Diagrama e modificação da classe Endereco

--- src/br/com/projecao/evento/model/Usuario.java
+++ src/br/com/projecao/evento/model/Usuario.java
@@ -1,9 +1,62 @@
 package br.com.projecao.evento.model;
 
-import java.sql.Date; 
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class Usuario {
-	
 
-			
+	private String nome;
+	private String cpf;
+	private Endereco endereco;
+	private String email;
+	private String telefone;
+	private LocalDate dataNascimento;
+
+	public String getNome() {
+		return nome;
 	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+}
